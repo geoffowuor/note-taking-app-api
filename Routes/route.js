2c57e306aaada40dca6a5e58904bbff07780dd37
geoffowuor <geoffreygoovy@gmail.com>
2025-06-29 09:04:29 +0300

    added filter by id function

--- Routes/route.js
+++ Routes/route.js
@@ -9,12 +9,12 @@ router.post('/notes', async (req,res) => {
         await note.save();
         res.status(201).send(note);
     }catch(error){
-        res.status(400).send(error);
+        res.status(500).send(error);
     }
     
 });
 
-// list
+// list all notes
 router.get('/notes', async (req,res) => {
     try{
         const note = await Note.find();
@@ -24,6 +24,18 @@ router.get('/notes', async (req,res) => {
     }
     
 });
+
+// list by id
+router.get('/notes/:id', async (req,res) => {
+    try{
+        const note = await Note.findById();
+        if (!note) return res.status(404).send();
+        res.send(note);
+    }catch(error){
+        res.status(500).send(error);
+    }
+    
+})
 
 
 
