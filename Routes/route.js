e3401377fb8c1d1be49735829a62cc3d7ddaabde
geoffowuor <geoffreygoovy@gmail.com>
2025-06-29 09:12:20 +0300

    added update endpoint

--- Routes/route.js
+++ Routes/route.js
@@ -28,16 +28,26 @@ router.get('/notes', async (req,res) => {
 // list by id
 router.get('/notes/:id', async (req,res) => {
     try{
-        const note = await Note.findById();
+        const note = await Note.findById(req.params.id);
         if (!note) return res.status(404).send();
         res.send(note);
     }catch(error){
         res.status(500).send(error);
     }
     
-})
+});
 
-
+//update
+router.put('/notes/:id', async (req,res) => {
+    try{
+        const note = await Note.findByIdAndUpdate(req.params.id);
+        if (!note) return res.status(404).send();
+        res.send(note);
+    }catch(error){
+        res.status(500).send(error);
+    }
+    
+});
 
 
 
